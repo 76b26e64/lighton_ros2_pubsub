7896e29b919c37cc12756d3655ba4f2ae35cefc1
Oreno <kanekosho@Kanekos-MacBook-Pro-4.local>
2026-05-06 17:38:31 -0700

    modified to refer controller_lib

---
+++ include/lighton_ros2_pubsub/pubsub.hpp
@@ -0,0 +1,12 @@
+#ifndef PUBSUB_HPP_ 
+#define PUBSUB_HPP_ 
+
+#define TOPIC_NAME "light_command"
+#define PUBLISHER_TRY_MAX   (10)
+#define PUBLISHER_FIND_WAIT_MSEC (500ms)
+#define PUBLISHER_ACK_WAIT_SEC   (5)
+#define PUBLISHER_KEEP_ALIVE_MSEC (500ms)
+
+#endif  // PUBSUB_HPP_ 
+
+
