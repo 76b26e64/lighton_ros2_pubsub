edf729ae77ba4547f6f4dad8bef97fc4c9fca051
Sho Kaneko <kamisugi632@gmail.com>
2026-08-28 01:32:26 -0700

    new file:   .vscode/settings.json 	modified:   CMakeLists.txt 	modified:   package.xml 	modified:   src/publisher.cpp 	modified:   src/subscriber.cpp

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -4,6 +4,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "lighton_ros2_pubsub/pubsub.hpp"
 
 using namespace std::chrono_literals;
 
@@ -12,19 +13,36 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<rclcpp::Node>("one_shot_publisher");
-  auto publisher = node->create_publisher<std_msgs::msg::String>("topic", 10);
+  auto publisher = node->create_publisher<std_msgs::msg::String>(TOPIC_NAME, rclcpp::QoS(1).reliable());
 
-  // subscriber discoveryのため少し待つ
-  rclcpp::sleep_for(500ms);
+  for(int i {0}; i < PUBLISHER_TRY_MAX; i++){
+    if (publisher->get_subscription_count() != 0) {
+      break;
+    }
+    RCLCPP_INFO(node->get_logger(), "Waiting for subscriber...");
+    rclcpp::sleep_for(PUBLISHER_FIND_WAIT_MSEC);
+  }
+    
+  if (publisher->get_subscription_count() == 0) {
+    RCLCPP_ERROR(node->get_logger(), "Subscriber not found.");
+    rclcpp::shutdown();
+    return -1;
+  }
 
   std_msgs::msg::String message;
   message.data = "Hello, ROS2! Publish once.";
 
-  RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message.data.c_str());
+  RCLCPP_INFO(node->get_logger(), "Publish: '%s'", message.data.c_str());
   publisher->publish(message);
 
-  // 送信処理が流れるよう少し待つ
-  rclcpp::sleep_for(200ms);
+  // Wait until all matched subscribers ACK the message
+  if (publisher->wait_for_all_acked(std::chrono::seconds(PUBLISHER_ACK_WAIT_SEC))) {
+    RCLCPP_INFO(node->get_logger(), "Message delivered to subscribers");
+  } else {
+    RCLCPP_WARN(node->get_logger(), "Timeout waiting for ACKs");
+  }
+    
+  rclcpp::sleep_for(PUBLISHER_KEEP_ALIVE_MSEC);
 
   rclcpp::shutdown();
   return 0;
--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -3,67 +3,29 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-#include <iostream>
-#include <thread>
-#include <chrono>
-#include <algorithm>
-#include <pigpiod_if2.h>
+#include "controller.hpp"
+#include "rpi3_light_controller.hpp"
+#include "lighton_ros2_pubsub/pubsub.hpp"
 
-// 角度(0-180) → パルス幅(µs) に変換
-// SG90の典型: 500us〜2500us を 0〜180度に割り当て（個体差あり）
-static int angle_to_pulse_us(int angle)
-{
-  angle = std::clamp(angle, 0, 180);
-  const int min_us = 500;  // 0°
-  const int max_us = 2500; // 180°
-  return min_us + (max_us - min_us) * angle / 180;
+using namespace std;
+
+namespace {
+  static unique_ptr<controller_lib::Controller> controller;
 }
 
-class MinimalSubscriber : public rclcpp::Node
+class Subscriber : public rclcpp::Node
 {
 public:
-  MinimalSubscriber()
-      : Node("minimal_subscriber")
+  Subscriber() : Node("controller_subscriber")
   {
     auto topic_callback =
         [this](std_msgs::msg::String::UniquePtr msg) -> void
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-      const int gpio = 18; // 信号線をつなぐGPIO（例: GPIO18=物理12番ピン）
-      const int sleep_time = 3;
-
-      // pigpioデーモンに接続
-      int pi = pigpio_start(nullptr, nullptr);
-      if (pi < 0)
-      {
-        std::cerr << "pigpio_start failed. Is pigpiod running?\n";
-        std::cerr << "Try: sudo systemctl start pigpiod\n";
-        return;
-      }
-
-      // GPIOを出力に（必須ではないが明示）
-      set_mode(pi, gpio, PI_OUTPUT);
-      auto move_to = [&](int angle)
-      {
-        int pulse = angle_to_pulse_us(angle);
-        // パルス幅をµsで指定（50Hz相当のサーボ制御）
-        set_servo_pulsewidth(pi, gpio, pulse);
-        // std::cout << "Angle " << angle << " deg -> " << pulse << " us\n";
-      };
-
-      move_to(90);
-      std::this_thread::sleep_for(std::chrono::seconds(sleep_time));
-      move_to(60);
-      std::this_thread::sleep_for(std::chrono::seconds(sleep_time));
-      move_to(90);
-      std::this_thread::sleep_for(std::chrono::seconds(sleep_time));
-
-      // 出力停止（0を指定するとサーボ信号OFF）
-      set_servo_pulsewidth(pi, gpio, 0);
-      pigpio_stop(pi);
+      RCLCPP_INFO(this->get_logger(), "Subscribe: '%s'", msg->data.c_str());
+      controller->on();
     };
 
-    subscription_ = this->create_subscription<std_msgs::msg::String>("topic", 10, topic_callback);
+    subscription_ = this->create_subscription<std_msgs::msg::String>(TOPIC_NAME, rclcpp::QoS(1).reliable(), topic_callback);
   }
 
 private:
@@ -72,8 +34,14 @@ private:
 
 int main(int argc, char *argv[])
 {
+  controller = controller_lib::Rpi3LightController::create();
+  if(!controller){
+    cerr << "Error controller can't create." << endl;
+    return -1;
+  }
+  
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<Subscriber>());
   rclcpp::shutdown();
   return 0;
 }
